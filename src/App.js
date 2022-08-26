e609f6f36a74b26c126b4e96057af4d4d6f8a35d
GiladLev <giladlev2000@gmail.com>
2022-08-26 15:21:17 +0300

    v2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header";
+import TodoForm from "./components/TodoForm";
+import List from "./components/List";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+    const [todos, setTodos]= useState([])
+    const [name, setName]= useState("Gilad")
+
+    function fillTodoList(todo, setTodo) {
+      // const todoList = Object.assign([],todos)
+      // ... פירוק המערך לגורמים והכנסה למערך
+      const todoList= [...todos]
+      todoList.push(todo)
+      setTodos(todoList)
+      setTodo("")
+    }
+
+
+    return (
+      <div className="App">
+  
+        <Header myName={name}/>
+        <TodoForm fillTodoList={fillTodoList}/>
+        <List todos={todos}/>
+        
+
+
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/MXtIpoh6w8w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
-  );
-}
-
-export default App;
+    );
+  };
+  
+  export default App;
